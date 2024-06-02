622c535c771fdeb65a1ffb88eb28eb45d3a7392a
dekolor <stefanbriceag1@gmail.com>
2024-06-03 00:29:35 +0300

    Created some basic UI

---
+++ app/Http/Controllers/EmailAddressController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmailAddressController extends Controller
+{
+    //
+    public function list() {
+        return view('address.list');
+    }
+}
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('address.list')" :active="request()->routeIs('address.list')">
+                        {{ __('Email Addresses') }}
+                    </x-nav-link>
                 </div>
             </div>
 
